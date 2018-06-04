9f679bf381beb7af4cafa82e60c72cb7cd234771
BOTQWEREW123 <39924208+BOTQWEREW123@users.noreply.github.com>
2018-06-04 22:00:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,18 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const ytdl = require('ytdl-core');
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+
+
 client.on('message', message => {
   if (message.content.startsWith('مهرجان فساد العالم')) {
     const voiceChannel = message.member.voiceChannel;
@@ -15,4 +27,4 @@ client.on('message', message => {
   }
 })
 
-client.login('NDI2NzU2OTU2MTE2NzQ2MjUy.DZan8g.pWZwtE4y-RLoZr9ZgTRcC5JwiL4');
+client.login('NDI2MTMwMDE5MTE5NTI5OTg0.DZRgcQ.8eTtjzShtq0BWZD6VPi0Zw-ZT8s');
